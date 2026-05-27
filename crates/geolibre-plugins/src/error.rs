d95dfaa5eafede2d233c1e4017ca238d42cf0c2b
Qiusheng Wu <giswqs@gmail.com>
2026-05-27 18:32:13 -0400

    Remove Rust

--- crates/geolibre-plugins/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum PluginError {
-    #[error("plugin failed: {0}")]
-    Failed(String),
-}
-
-pub type Result<T> = std::result::Result<T, PluginError>;
